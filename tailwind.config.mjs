1ce5e8c800b8d0f037b4464d6ede26a6bc45ad1e
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-12-26 22:42:17 -0300

    add fonts to tailwind coonfig

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,4 +5,17 @@ export default {
 		extend: {},
 	},
 	plugins: [],
+	fontFamily: {
+		sans: ['Graphik', 'sans-serif'],
+		serif: ['Merriweather', 'serif'],
+	},
+	colors: {
+		'blue': '#1fb6ff',
+		'pink': '#ff49db',
+		'orange': '#ff7849',
+		'green': '#13ce66',
+		'gray-dark': '#273444',
+		'gray': '#8492a6',
+		'gray-light': '#d3dce6',
+	},
 }
